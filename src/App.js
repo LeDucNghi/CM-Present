92c0e7b65d0ffaf1940dc3f65fce2922e0deab72
kendumboiz <leducnghi28122000@gmail.com>
2022-06-09 14:04:18 +0700

    setup src

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import React, { Suspense } from "react";
 
 import { Loading } from "components/Loading";
+import SignIn from "pages/SignIn/signin";
+import SignUp from "pages/SignUp/signup";
 
 const Home = React.lazy(() => {
   return new Promise((resolve) => {
@@ -33,6 +35,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="home" />} />
+
+        <Route path="/signin" element={<SignIn />} />
+
+        <Route path="/signup" element={<SignUp />} />
 
         <Route
           index
